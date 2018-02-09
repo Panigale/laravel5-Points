7b181348ebca50dc031322dba5d57c1fbbdb6f63
davy's Macbook pro <davy123498@gmail.com>
2018-02-09 13:11:22 +0800

    fixed when point not isset still can update number problem.

--- src/Traits/AddPoint.php
+++ src/Traits/AddPoint.php
@@ -13,14 +13,73 @@ use Panigale\Point\Models\PointIncrease;
 
 trait AddPoint
 {
+    /**
+     * 點數 Model
+     *
+     * @var Point
+     */
+    protected $point;
+
+    /**
+     * rule id
+     *
+     * @var int
+     */
+    protected $ruleId;
+
     protected function addPointToUser(int $ruleId, $number, $beforePoint, $afterPoint)
     {
+        $this->ruleId = $ruleId;
         $this->logPoint(new PointIncrease(), $ruleId, $number, $beforePoint, $afterPoint);
+
+        if($this->pointNotIsset())
+            $this->createPointToUser();
 
         return Point::where('user_id' ,$this->id)
                     ->where('rule_id' ,$ruleId)
                     ->update([
                         'number' => $afterPoint
                     ]);
+    }
+
+    protected function createPointToUser($number = null)
+    {
+        Point::create([
+            'user_id' => $this->id,
+            'rule_id' => $this->ruleId,
+            'number' => $number
+        ]);
+    }
+
+    /**
+     * 檢查使用者是否擁有這個點數
+     *
+     * @return mixed
+     */
+    protected function pointNotIsset()
+    {
+        $point = Point::where('user_id' ,$this->id)
+                      ->where('rule_id' , $this->ruleId)
+                      ->first();
+
+        if(is_null($point))
+            return false;
+
+        $this->setPoint($point);
+
+        return $point;
+    }
+
+    /**
+     * 設定目前在執行的 Point Model
+     *
+     * @param Point $point
+     * @return $this
+     */
+    protected function setPoint(Point $point)
+    {
+        $this->point = $point;
+
+        return $this;
     }
 }
